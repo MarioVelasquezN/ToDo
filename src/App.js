0dad22fc17d770a282c32c999717083927e4a040
MarioVelasquezN <rolando.velasquez18@hotmail.com>
2022-04-09 00:15:21 -0600

    finalconerrorenfilter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import Todo from './components/Todo';
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from './components/FilterButton';
 import {nanoid} from "nanoid";
 
 
+const FILTER_MAP={
+  ALL:()=>true,
+  Active: task=>!task.completed,
+  Completed: task=>task.completed
+  
+};
+const FILTER_NAMES=Object.keys(FILTER_MAP);
 
 function App(props) {
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+  
+  
+
   const [tasks, setTasks]= useState(props.tasks);
+  const [filter,setFilter]= useState('All');
+  
 
   function addTask(name){
     const newTask= { id:"todo-"+nanoid(), name: name, completed: false};
@@ -41,32 +61,54 @@ function App(props) {
     setTasks(editTaskList);
   }
 
-  const taskList= tasks.map(task=>(
-      <Todo 
-        id={task.id} 
-        name={task.name} 
-        completed={task.completed}
-        key={task.id} 
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-        editTask={editTask}
-      />
-    )
-  );
+  
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  
+
+
+  const filterList=FILTER_NAMES.map(name=>(
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name===filter}
+      setFilter={setFilter}
+    />
+  ));
 
   const tasksNoun= taskList.length !==1? 'tasks': 'task';
   const headingText=`${taskList.length} ${tasksNoun} remaining`;
+  
+
+  const listHeadingRef=useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+  
 
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton/>
-        <FilterButton/>
-        <FilterButton/>
+        {filterList}
       </div>
-      <h2 id="list-heading">
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
         {headingText}
       </h2>
       <ul
